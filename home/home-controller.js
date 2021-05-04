8f1adf247f2435d932ed5e99e734383710f96389
Toshio Watanabe <toshio_ws@hotmail.com>
2021-05-03 22:58:07 -0300

    Feat: list

--- home/home-controller.js
+++ home/home-controller.js
@@ -4,16 +4,17 @@ HomeController.$inject = ['$location', 'CursoService'];
     function HomeController($location, CursoService) {
         vm = this;
         vm.titulo = 'Home';
-        vm.clientes = '';
+        vm.clientes = [];
 
         vm.navegar = function() {
             $location.path('cadastro')
         }
 
         vm.listarClientes = function() {
-            CursoService.exec_GET().then(function(responsta){
-                if(responsta) {
-                    console.log(responsta);
+            CursoService.exec_GET().then(function(response){
+                if(response) {
+                   vm.clientes = response;
+                   //console.log(response);
                 }
             })
         }
